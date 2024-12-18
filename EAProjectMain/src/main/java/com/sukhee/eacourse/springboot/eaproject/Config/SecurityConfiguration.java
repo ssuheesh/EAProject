d9a6e75af9d307a5876f6ff4d630c90d865ec47b
Sukhbat A - laptop <ssuheesh@gmail.com>
2024-12-18 13:24:26 -0600

    projects divided

--- EAProjectMain/src/main/java/com/sukhee/eacourse/springboot/eaproject/Config/SecurityConfiguration.java
+++ EAProjectMain/src/main/java/com/sukhee/eacourse/springboot/eaproject/Config/SecurityConfiguration.java
@@ -8,9 +8,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,7 +21,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
     @Autowired
     private TokenProvider tokenProvider;
@@ -26,14 +30,14 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(CsrfConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(new JWTFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/users/register-admin").permitAll()
                         .requestMatchers("/api/users/register").permitAll() // Allow /register endpoint
                         .requestMatchers("/api/users/login").permitAll()   // Allow /login endpoint
-                        .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
                         .anyRequest().authenticated()                      // All other requests require authentication
                 );
         return http.build();
--- EAProjectMain/src/main/java/com/sukhee/eacourse/springboot/eaproject/Controller/AdminController.java
+++ EAProjectMain/src/main/java/com/sukhee/eacourse/springboot/eaproject/Controller/AdminController.java
@@ -3,6 +3,7 @@ package com.sukhee.eacourse.springboot.eaproject.Controller;
 import com.sukhee.eacourse.springboot.eaproject.Domain.User;
 import com.sukhee.eacourse.springboot.eaproject.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class AdminController {
     @Autowired
     private UserService userService;
     @GetMapping("/users")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<User> getAllUsers() {
         return userService.findAllUsers();
     }
